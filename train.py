31cb57596c295d3090c4d55bcc277edb82946c15
moonsub <m.kwon@xiilab.com>
2024-03-19 10:18:43 +0900

    remaining time k8s annotation에 저장, epoch log db저장코드

--- train.py
+++ train.py
@@ -1,29 +1,32 @@
-import os
 import argparse
-from ultralytics import YOLO
+import os
 
-#model freeze
+from ultralytics import YOLO
+from ultralytics.utils.astrago_db import MariaDBHandler
+
+
+# model freeze
 def freeze_layer(trainer):
     model = trainer.model
     num_freeze = 10
     print(f"Freezing {num_freeze} layers")
     freeze = [f'model.{x}.' for x in range(num_freeze)]  # layers to freeze 
-    for k, v in model.named_parameters(): 
+    for k, v in model.named_parameters():
         v.requires_grad = True  # train all layers 
-        if any(x in k for x in freeze): 
-            print(f'freezing {k}') 
-            v.requires_grad = False 
+        if any(x in k for x in freeze):
+            print(f'freezing {k}')
+            v.requires_grad = False
     print(f"{num_freeze} layers are freezed.")
 
 
-#save dir filter same folder
+# save dir filter same folder
 def make_unique_folder(path):
     path = path.rstrip("/")
     folder_name = os.path.basename(path)
     folder_path = os.path.dirname(path)
     new_folder_path = path
     i = 1
-    while os.path.exists(new_folder_path): 
+    while os.path.exists(new_folder_path):
         new_folder_name = f"{folder_name}_{i}"
         new_folder_path = os.path.join(folder_path, new_folder_name)
         i += 1
@@ -31,26 +34,26 @@ def make_unique_folder(path):
     return new_folder_path
 
 
-def trainer(config) :
+def trainer(config):
     new_folder = make_unique_folder(config['save_model_dir'])
     print(f"New folder created: {new_folder}")
 
     # Load a model
-    #model = YOLO('yolov8l.yaml')  # build a new model from YAML
-    #model = YOLO('yolov8l.pt')  # load a pretrained model (recommended for training)
+    # model = YOLO('yolov8l.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8l.pt')  # load a pretrained model (recommended for training)
     model = YOLO(config['model']).load(config['model_pt'])  # build from YAML and transfer weights
     model.add_callback("on_train_start", freeze_layer)
 
     # Train the model
     results = model.train(
         data=config['data_dir'],
-        imgsz=config['image_size'],        
+        imgsz=config['image_size'],
         epochs=config['epochs'],
         batch=config['batch_size'],
         lr0=config['learning_rate'],
         lrf=config['learning_rate'],
-        save_dir=new_folder,
-        
+        project=new_folder,
+
         device=[0],
         workers=config['worker'],
         optimizer=config['opt'],
@@ -60,26 +63,51 @@ def trainer(config) :
         patience=config['patience'],
     )
     model.val()
+    db_handler = MariaDBHandler(
+        host='10.61.3.12',
+        port='30756',
+        user='root',
+        password='root',
+        database='astrago'
+    )
+    db_handler.connect()
+    parameter_id = db_handler.insert_prediction_parameter(('yolov8l.yaml', '../weights/yolov8l.pt',
+                                                           '../astrago-ultralytics-yolov8-train/ultralytics/cfg/datasets/coco128.yaml',
+                                                           640, 100, 16, 0.01, '../detect/run', 0, 8, 'SGD', True, 0.0,
+                                                           False))
+    db_handler.insert_epoch_log(parameter_id, ('yolov8l.yaml', 8, 'TESLA V100', 235, 8,  # CLASS_NUM
+                                               8, 8, 8, 8, 360,  # IMG_SIZE
+                                               30, 1, 10, 10, 10,  # VALID_TIME
+                                               10, 10, 10, 10, 10,  # REMAINING_TIME
+                                               3, 10
+                                               ))
+    db_handler.disconnect()
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='YOLOv8 training')
-    #user setting
-    parser.add_argument('--data_dir', type=str, default="../astrago-ultralytics-yolov8-train/ultralytics/cfg/datasets/coco128.yaml", help="yaml file")    
+    # user setting
+    parser.add_argument('--data_dir', type=str,
+                        default="../astrago-ultralytics-yolov8-train/ultralytics/cfg/datasets/coco128.yaml",
+                        help="yaml file")
     parser.add_argument('--image_size', type=int, default=640, help="input image scale")
     parser.add_argument('--epochs', type=int, default=100, help="num epochs")
     parser.add_argument('--batch_size', type=int, default=16, help="number of images per batch (-1 for AutoBatch)")
     parser.add_argument('--learning_rate', type=float, default=0.01, help="output checkpoint, info save_dir")
     parser.add_argument('--save_model_dir', type=str, default="../detect/run", help="output checkpoint, info save_dir")
-    
+
     parser.add_argument('--model', type=str, default="yolov8l.yaml", help="model .yaml default yolov8n.yaml")
     parser.add_argument('--model_pt', type=str, default="../weights/yolov8l.pt", help="pre-trained model path")
-    parser.add_argument('--patience', type=int, default=0, help='EarlyStopping patience (epochs without improvement)')    
-    parser.add_argument('--worker', type=int, default=8, help="number of worker threads for data loading (per RANK if DDP)")
+    parser.add_argument('--patience', type=int, default=0, help='EarlyStopping patience (epochs without improvement)')
+    parser.add_argument('--worker', type=int, default=8,
+                        help="number of worker threads for data loading (per RANK if DDP)")
     parser.add_argument('--pretrained', type=bool, default=False, help="whether to use a pretrained model")
-    parser.add_argument('--opt', type=str, default='SGD', help="optimizer to use, choices=['SGD', 'Adam', 'AdamW', 'RMSProp']")
+    parser.add_argument('--opt', type=str, default='SGD',
+                        help="optimizer to use, choices=['SGD', 'Adam', 'AdamW', 'RMSProp']")
     parser.add_argument('--single_cls', action='store_true', help='train multi-class data as single-class')
     parser.add_argument('--label_smoothing', type=float, default=0.0, help='Label smoothing epsilon')
     return vars(parser.parse_args())
+
 
 if __name__ == "__main__":
     args = parse_args()
--- ultralytics/utils/astrago_db.py
+++ ultralytics/utils/astrago_db.py
@@ -1,10 +1,34 @@
 import os
+
 import mysql.connector
 from kubernetes import client, config
 
-class kubernetes_info :
 
-    def get_node_port :
+class KubernetesInfo:
+
+    def __init__(self):
+        config.load_kube_config()
+        self.pod_name = os.environ.get("POD_NAME")
+        self.pod_namespace = os.environ.get("POD_NAMESPACE")
+        self.api_instance = client.CoreV1Api()
+
+    def change_estimated_remaining_time(self, estimated_remaining_time):
+        try:
+            # Pod 조회
+            pod = self.api_instance.read_namespaced_pod(name=self.pod_name, namespace=self.pod_namespace)
+
+            # 기존 annotation에 새로운 값을 추가 또는 업데이트
+            if pod.metadata.annotations is None:
+                pod.metadata.annotations = {}
+            pod.metadata.annotations['estimated_remaining_time'] = str(estimated_remaining_time)
+
+            # Pod 업데이트
+            self.api_instance.patch_namespaced_pod(name=self.pod_name, namespace=self.pod_namespace, body=pod)
+            print("Pod의 annotation이 성공적으로 업데이트되었습니다.")
+        except Exception as e:
+            print(f"Pod annotation 업데이트 중 오류 발생: {e}")
+
+    def get_node_port(self):
         # Kubernetes 클러스터 설정
         config.load_incluster_config()
         v1 = client.CoreV1Api()
@@ -26,93 +50,126 @@ class kubernetes_info :
         print(f"서비스 이름: {service_name}")
         print(f"노드 포트: {node_port}")
 
-class db :
 
-    def connection_db():
-        # MariaDB에 연결
-        astra_db = mysql.connector.connect(
-        host="10.61.3.12",
-        port=int(30756),
-        user="root",
-        password="root",
-        database="astrago"
-        )
+class MariaDBHandler:
+    def __init__(self, host, port, user, password, database):
+        """
+        MariaDBHandler 클래스의 생성자 메서드입니다.
 
-        return astra_db
-    def insert_prediction_parameter(astra_db):
-        #TB_JOB_PREDICTION_PARAMETER
-        cursor = astra_db.cursor()
-        sql_insert = f"""
-                    INSERT INTO TB_JOB_PREDICTION_PARAMETER
-                    (
-                        MODEL
-                        ,MODEL_PT
-                        ,DATA_DIR
-                        ,IMAGE_SIZE
-                        ,EPOCH
-                        ,BATCH_SIZE
-                        ,LEARNING_RATE
-                        ,SAVE_MODEL_DIR
-                        ,PATIENCE
-                        ,WORKER
-                        ,OPT
-                        ,SINGLE_CLS
-                        ,LABEL_SMOOTHING
-                        ,PRETRAINED
-                    )
-                    VALUES
-                    (
-                        '{annotationSet}'
-                        ,"{data_obj['DATA_ID']}"
-                        ,"{data_obj['LABEL_ID']}"
-                        ,"{label_type}"
-                        ,"{last_worker_account}"
-                        ,"SYSTEM"
-                        ,DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d %T')
-                        ,"SYSTEM"
-                        ,DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d %T')
-                    )
-                    """
-        cursor.execute(sql_insert)
-        cursor.close()
-        astra_db.commit()
+        :param host: MariaDB 호스트 주소
+        :param user: MariaDB 사용자 이름
+        :param password: MariaDB 암호
+        :param database: 연결할 데이터베이스 이름
+        """
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.port = port
+        self.conn = None
+        self.cursor = None
 
-        pk_value = cursor.lastrowid
-        return pk_value
+    def connect(self):
+        """
+        MariaDB에 연결하는 메서드입니다.
+        """
+        try:
+            self.conn = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                port=self.port,
+                password=self.password,
+                database=self.database
+            )
+            self.cursor = self.conn.cursor()
+            print("MariaDB에 연결되었습니다.")
+        except mysql.connector.Error as e:
+            print(f"MariaDB 연결 오류: {e}")
 
-    def insert_EPOCH_LOG(astra_db):
-        #TB_JOB_PREDICTION_PARAMETER
-        cursor = astra_db.cursor()
-        sql_insert = f"""
-                    INSERT INTO TB_JOB_PREDICTION_EPOCH_LOG
-                    (
-                        JOB_PREDICTION_EPOCH_LOG_ID
-                        ,JOB_PREDICTION_PARAMETER_ID
-                        ,TOTAL_EPOCH
-                        ,CURRENT_EPOCH
-                        ,PREPROCESS_TIME
-                        ,VALIDATION_TIME
-                        ,SAVE_MODEL_TIME
-                        ,SCHEDULER_TIME
-                        ,EPOCH_PER_TIME
-                        ,PREDICTION_END_TIME
-                        ,ELAPSED_TIME
-                        ,GPU_USAGE
-                        ,CPU_USAGE
-                    )
-                    VALUES
-                    (
-                        '{annotationSet}'
-                        ,"{data_obj['DATA_ID']}"
-                        ,"{data_obj['LABEL_ID']}"
-                        ,"{label_type}"
-                        ,"{last_worker_account}"
-                        ,"SYSTEM"
-                        ,DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d %T')
-                        ,"SYSTEM"
-                        ,DATE_FORMAT(CURRENT_TIMESTAMP, '%Y-%m-%d %T')
-                    )
-                    """
-        cursor.execute(sql_insert)
-        cursor.close()
-        astra_db.commit()
+    def disconnect(self):
+        """
+        MariaDB 연결을 해제하는 메서드입니다.
+        """
+        if self.conn.is_connected():
+            self.cursor.close()
+            self.conn.close()
+            print("MariaDB 연결이 닫혔습니다.")
+
+    def insert_epoch_log(self, parameter_id, values):
+        """
+        TB_JOB_PREDICTION_EPOCH_LOG 테이블에 데이터를 삽입하는 메서드입니다.
+
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            INSERT INTO TB_JOB_PREDICTION_EPOCH_LOG 
+            (JOB_PREDICTION_PARAMETER_ID, MODEL_NAME, MODEL_PARAM_NUM,
+             GPU_TYPE, GPU_FLOPS, CLASS_NUM, TRAIN_IMG_NUM, TRAIN_INSTANCE_NUM, VALID_IMG_NUM, VALID_INSTANCE_NUM,
+             IMG_SIZE, BATCH_SIZE, EPOCH_CNT, PREPROCESS_TIME, TRAIN_TIME, VALID_TIME, TIME_PER_EPOCH, SCHEDULER_TIME,
+             EPOCH_TIME, ELAPSED_TIME, REMAINING_TIME, GPU_USAGE, CPU_USAGE) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # 데이터 삽입
+            self.cursor.execute(sql, (parameter_id,) + values)
+
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 삽입되었습니다.")
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+
+    def insert_prediction_parameter(self, values):
+        """
+        TB_JOB_PREDICTION_PARAMETER 테이블에 데이터를 삽입하는 메서드입니다.
+
+        :param values: 삽입할 데이터 값
+        """
+        try:
+            # 데이터 삽입 쿼리 생성
+            sql = """
+            INSERT INTO TB_JOB_PREDICTION_PARAMETER 
+            (MODEL, MODEL_PT, DATA_DIR, IMAGE_SIZE, EPOCH, BATCH_SIZE, LEARNING_RATE,
+             SAVE_MODEL_DIR, PATIENCE, WORKER, OPT, SINGLE_CLS, LABEL_SMOOTHING, PRETRAINED) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # 데이터 삽입
+            self.cursor.execute(sql, values)
+
+            # 변경사항 커밋
+            self.conn.commit()
+            print("데이터가 성공적으로 삽입되었습니다.")
+            return self.cursor.lastrowid
+        except Exception as e:
+            print(f"데이터 삽입 중 오류 발생: {e}")
+            # 롤백
+            self.conn.rollback()
+
+
+if __name__ == '__main__':
+    client = KubernetesInfo()
+    client.change_estimated_remaining_time(10)
+    db_handler = MariaDBHandler(
+        host='10.61.3.12',
+        port='30756',
+        user='root',
+        password='root',
+        database='astrago'
+    )
+    db_handler.connect()
+    parameter_id = db_handler.insert_prediction_parameter(('yolov8l.yaml', '../weights/yolov8l.pt',
+                                                           '../astrago-ultralytics-yolov8-train/ultralytics/cfg/datasets/coco128.yaml',
+                                                           640, 100, 16, 0.01, '../detect/run', 0, 8, 'SGD', True, 0.0,
+                                                           False))
+    db_handler.insert_epoch_log(parameter_id, ('yolov8l.yaml', 8, 'TESLA V100', 235, 8,  # CLASS_NUM
+                                               8, 8, 8, 8, 360,  # IMG_SIZE
+                                               30, 1, 10, 10, 10,  # VALID_TIME
+                                               10, 10, 10, 10, 10,  # REMAINING_TIME
+                                               3, 10
+                                               ))
+    db_handler.disconnect()
