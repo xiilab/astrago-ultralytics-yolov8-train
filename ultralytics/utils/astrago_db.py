42d4c76c96095627a12f920047ed34e1c15548db
leeyoungchun-xiilab <yc.lee@xiilab.com>
2024-05-22 13:55:47 +0900

    fix : 오류 수정

--- ultralytics/utils/astrago_db.py
+++ ultralytics/utils/astrago_db.py
@@ -178,9 +178,9 @@ class MariaDBHandler:
             """
             # 데이터 삽입
             print("남은 시간 데이터 업데이트 쿼리 : " + sql)
-            print("남은 시간 데이터 업데이트 쿼리 파라미터 time : " + time)
+            print("남은 시간 데이터 업데이트 쿼리 파라미터 time : " + str(time))
             print("남은 시간 데이터 업데이트 쿼리 파라미터 workloadResourceName : " + workloadResourceName)
-            self.cursor.execute(sql, (str(time), workloadResourceName))
+            self.cursor.execute(sql, (time, workloadResourceName))
             # 변경사항 커밋
             self.conn.commit()
             print("데이터가 성공적으로 업데이트 되었습니다.")
